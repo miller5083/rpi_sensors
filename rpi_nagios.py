6a88ea5364102d6184d074f50d9551c2bb102fca
Stefan Süss <stefan.suess89@gmail.com>
2014-03-12 11:30:16 +0100

    fixed typo

--- rpi_nagios.py
+++ rpi_nagios.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 ###############################################################################
-# Stefan SüAss www.sysstem.at
+# Stefan Süss - www.sysstem.at
 ###############################################################################
 
 import rpi_version, rpi_sensors
